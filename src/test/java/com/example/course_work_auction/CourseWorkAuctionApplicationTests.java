2b6f92088d3e05338df20c3e8856eaccdb901b36
helloworld <sergeychernonosov21229@gmail.com>
2023-03-19 20:07:27 +0300

    initial commit

---
+++ src/test/java/com/example/course_work_auction/CourseWorkAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_work_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
